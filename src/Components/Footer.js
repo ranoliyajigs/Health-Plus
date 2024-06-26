0c25d760b961a3495fde987a4294894f4b5b9853
Jigar Ranoliya <maheshranoliya1@gmail.com>
2024-06-26 12:09:02 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,144 @@
+import React from "react";
+import "../Styles/Footer.css";
+import SubscribeNewsletter from "./SubscribeNewsletter";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-section">
+      <div className="footer-container">
+        <div className="ft-info">
+          <div className="ft-info-p1">
+            <p className="ft-title">
+              Health <span className="ft-sign">+</span>
+            </p>
+            <p className="ft-description">
+              Talk to online doctors and get medical advice, online
+              prescriptions, refills and medical notes within minutes. On-demand
+              healthcare services at your fingertips.
+            </p>
+          </div>
+
+          <SubscribeNewsletter />
+        </div>
+
+        <div className="ft-list">
+          <p className="ft-list-title">Services</p>
+          <ul className="ft-list-items">
+            <li>
+              <a href="#services">Emergency Care</a>
+            </li>
+            <li>
+              <a href="#services">Heart Disease</a>
+            </li>
+            <li>
+              <a href="#services">Dental Care</a>
+            </li>
+            <li>
+              <a href="#services">Prescription</a>
+            </li>
+            <li>
+              <a href="#services">Insights for doctors</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ft-list">
+          <p className="ft-list-title">Legal</p>
+          <ul className="ft-list-items">
+            <li>
+              <Link to={"/legal"}>General Info</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Terms of Services</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Consultations</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>How it Works</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ft-list" id="contact">
+          <p className="ft-list-title">Talk To Us</p>
+          <ul className="ft-list-items">
+            <li>
+              <a href="mailto:support@healthplus.com">support@healthplus.com</a>
+            </li>
+            <li>
+              <a href="mailto:appointment@healthplus.com">
+                appointment@healthplus.com
+              </a>
+            </li>
+            <li>
+              <a href="tel:+022 5454 5252">+022 5454 5252</a>
+            </li>
+            <li>
+              <a href="tel:+022 2326 6232">+022 2326 6232</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="ft-copyright">
+        <p>© 2013-2023 Health+. All rights reserved.</p>
+
+        <ul className="ft-social-links">
+          <li>
+            <a
+              href="https://linkedin.com/in/jigar-ranoliya/"
+              title="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+              >
+                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
+              </svg>
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://facebook.com/"
+              title="FaceBook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 320 512"
+              >
+                <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+              </svg>
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://github.com/ranoliyajigs"
+              title="Twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
+    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+</svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
